3d45358f5084a2607d2ce02eb1da780bf751815a
Josh Pollock <jpollock412@gmail.com>
2013-12-11 17:37:07 -0500

    change classes and IDs to make this work.

--- js/yt1300.topbar.js
+++ js/yt1300.topbar.js
@@ -1,42 +1,46 @@
-$(function(){
-    $('#header_nav').data('size','big');
-});
 
-$(window).scroll(function(){
-    var $nav = $('#header_nav');
-    if ($('body').scrollTop() > 0) {
-        if ($nav.data('size') == 'big') {
-            $('#top-bar-widgets').css('display', 'none');
-            $('#header-menu').css({
-                display: 'inline',
-                top: '0px',
-            });
-            $('#header-logo').fadeOut("fast");
-            $('#header-menu').animate({
-                paddingLeft: '25%',
-            }), {queue:false, duration:600};
-            $('#header-menu').css('top', '0px');
-            $nav.data('size','small').stop().animate({
-                height:'50px'
-            }, 600);
-            $('#header-logo').animate({
-                left:200, opacity:"show"}, 600);
-        };
-    }
-    else {
-        if ($nav.data('size') == 'small') {
-            $('#top-bar-widgets').css('display', 'inline');
-            $('#top-wrap').css('display', 'inline');
-            $('#header-menu').animate({
-                display: 'block',
-                top: '40px',
-            }), {queue:false, duration:600}; ;
-            $('#header-menu').animate({
-                paddingLeft: '0px',
-            }), {queue:false, duration:600};             $nav.data('size','big').stop().animate({
-                height:'100px'
-            }, 600);
+jQuery(document).ready(function($) {
 
+    $(function(){
+        $('#masthead').data('size','big');
+    });
+
+    $(window).scroll(function(){
+        var $nav = $('#masthead');
+        if ($('body').scrollTop() > 0) {
+            if ($nav.data('size') == 'big') {
+                $('#top-bar-widgets').css('display', 'none');
+                $('nav#primary-navigation').css({
+                    display: 'inline',
+                    top: '0px',
+                });
+                $('#header-main').fadeOut("fast");
+                $('nav#primary-navigation').animate({
+                    paddingLeft: '25%',
+                }), {queue:false, duration:600};
+                $('nav#primary-navigation').css('top', '0px');
+                $nav.data('size','small').stop().animate({
+                    height:'50px'
+                }, 600);
+                $('#header-main').animate({
+                    left:200, opacity:"show"}, 600);
+            };
         }
-    }
-});
+        else {
+            if ($nav.data('size') == 'small') {
+                $('#top-bar-widgets').css('display', 'inline');
+                $('nav#primary-navigation').animate({
+                    display: 'block',
+                    top: '40px',
+                }), {queue:false, duration:600}; ;
+                $('nav#primary-navigation').animate({
+                    paddingLeft: '0px',
+                }), {queue:false, duration:600};             $nav.data('size','big').stop().animate({
+                    height:'100px'
+                }, 600);
+
+            }
+        }
+    });
+
+}); //end jQuery noConflict wrapper
