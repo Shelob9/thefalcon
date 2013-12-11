930e167af5e8bc86610549eb35175c97b9005586
Josh Pollock <jpollock412@gmail.com>
2013-12-11 17:06:58 -0500

    register top bar widget area and enqueue scripts/ styles

--- inc/topbar.php
+++ inc/topbar.php
@@ -9,10 +9,20 @@ class topbar {
     }
 
     function script_style() {
-
+        wp_enqueue_script( 'yt1300-topbar', get_stylesheet_directory_uri().'/js/yt1300.topbar.js', array('jquery'), false, true );
+        wp_enqueue_style( 'yt300-topbar', get_stylesheet_directory_uri().'/css/yt1300.topbar.css' );
     }
 
     function top_widget() {
+        register_sidebar( array(
+            'name'          => __( 'Topbar', 'yt1300' ),
+            'id'            => 'sidebar-tb',
+            'description'   => __( 'A small sidebar area in the topbar, great for social profile links.', 'yt1300' ),
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</aside>',
+            'before_title'  => '<h1 class="widget-title">',
+            'after_title'   => '</h1>',
+        ) );
 
     }
 
