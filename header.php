aa0acb1879de0b9a2150c13b5abd6329272ec390
Josh Pollock <jpollock412@gmail.com>
2013-12-12 10:04:36 -0500

    put header into a function, returned !wp_is_mobile

--- header.php
+++ header.php
@@ -39,31 +39,6 @@
 		</a>
 	</div>
 	<?php endif; ?>
-
-	<header id="masthead" class="site-header" role="banner">
-		<div id="big-top">
-            <div class="header-main">
-                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-            </div>
-            <div id="top-bar-widgets">
-               <p> TOPBAR WIDGETS I AM!</p>
-            </div>
-        </div>
-
-        <nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
-            <h1 class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></h1>
-            <a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
-            <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'container_class' => 'topbar-menu', ) ); ?>
-            <div class="search-toggle">
-                <a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
-            </div>
-            <div id="search-container" class="search-box-wrapper hide">
-                <div class="search-box">
-                    <?php get_search_form(); ?>
-                </div>
-            </div>
-        </nav>
-
-	</header><!-- #masthead -->
+    <?php yt1300_header(); ?>
 
 	<div id="main" class="site-main">
